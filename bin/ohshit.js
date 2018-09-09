a0dfb848aed52cd951eebad6310af744b20d18a2
leifriksheim <riksheim.leif@gmail.com>
2018-09-09 08:43:48 +0200

    Revert

--- bin/ohshit.js
+++ bin/ohshit.js
@@ -17,7 +17,7 @@ if (!project.gitExist()) {
 
 async function run() {
   clear();
-  speak.normal(figlet.textSync("Shit!"));
+  speak.normal(figlet.textSync("Oh Shit Git!"));
 
   try {
     const { command } = await questions.init();
